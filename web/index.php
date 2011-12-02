75ac8663b6bebc726209782adbfa95564ed8d8e1
h1ll37 <h1ll37@hotmail.com>
2011-12-01 22:09:56 -0600

    php_lib uploaded and what not.
    Signed-off-by: h1ll37 <h1ll37@hotmail.com>

--- web/index.php
+++ web/index.php
@@ -3,12 +3,37 @@
 	<head>
 		<title>HillTing Minecraft Manager</title>
 		<link rel="stylesheet" type="text/css" href="./style/style.css" />
+		<?php
+			include("./php_lib/communicate.php");
+		?>
+		<script type="text/javascript">
+			timer = self.setInterval("blink()",1100);
+			color = true;
+			function blink() {
+				if(color) {
+					document.getElementById('blink').style.color="444444";
+					color = false;
+				} else if(!color) {
+					document.getElementById('blink').style.color="7fff0";
+					color = true;
+				}
+			}
+		</script>
 	</head>
 	<body>
+		<?php
+			$comm = new communicate();
+			if(isset($_POST['start_server'])) {
+				$comm->start();
+			}
+			elseif(isset($_POST['stop_server'])) {
+				$comm->stop();
+			}
+		?>
 		<div class="navigation">
 			<div class="links">
 				<a class="active" href="">home</a>
-				<a href="">permissons</a>
+				<a href="">permissions</a>
 				<a href="">settings</a>
 				<a href="">logs</a>
 			</div><!-- links -->
@@ -17,13 +42,16 @@
 			<div class="main">
 				<div class="entry">
 					<div class="title">Core Functions</div>
-					<button name="Start Server">Start Server</button>
+					<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+						<input type="submit" name="start_server" value="Start Server" />
+						<input type="submit" name="stop_server" value="Stop Server" />
+					</form>
 				</div><!-- entry -->
 			</div><!-- main -->
 			<div class="sub">
 				<div class="prompt">
-					<div id="log"></div>
-					<textarea></textarea>
+					<textarea id="output" readonly="readonly">Rawr</textarea>
+					<textarea id="input">&gt; say hello</textarea>
 				</div><!-- prompt -->
 			</div><!-- sub -->
 		</div><!-- container -->
--- web/testclient.php
+++ web/testclient.php
@@ -9,11 +9,13 @@
 	$socket = socket_create (AF_INET, SOCK_STREAM, SOL_TCP );
 	$address = "127.0.0.1";
 	$port = 1337;
-	$info = "start";
+	$info = "stuff\n";
+	$len = 7;
 	socket_connect($socket, $address, $port);
 	socket_send($socket, $info, strlen($info), MSG_DONTROUTE); 
 	$buf = socket_read($socket, 1024, PHP_NORMAL_READ);
 	echo $buf;
+	//socket_shutdown($socket);
 	socket_close($socket);
     ?>
   </body>
