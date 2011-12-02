159ebfa1ea3234818dfa6c51e15f477e3e258d87
TingPing <tngpng@gmail.com>
2011-12-02 00:34:09 -0500

    added exceptions and returns

--- python/main.py
+++ python/main.py
@@ -12,17 +12,22 @@ def server_start(memory='1024M', jar='../minecraft/minecraft_server.jar', cwd='.
         executable = 'java -Xmx' + memory + ' -Xms' + memory + ' -jar ' + jar + ' nogui'
         global serverproc
         serverproc = subprocess.Popen(executable, cwd=cwd, startupinfo=startupinfo, stdin=subprocess.PIPE, universal_newlines=True)
-
+        global returncode
+        returncode = serverproc.returncode
+        
 def server_comm(serverin):
     if (serverin == 'start'):
         server_start()
+        return 'started'
     elif (serverin == 'terminate'):
         serverproc.terminate()
         os.remove('../minecraft/server.log.lck')
+        return 'killed'
     else:
         serverproc.communicate(serverin)
+        return 'sent'
 
-command={'start':server_start(), 'terminate':serverproc.terminate()}
+#command={'start':server_start(), 'terminate':serverproc.terminate()}
 
 while True:
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -30,13 +35,15 @@ while True:
     serversocket.listen(5)
     connection, address = serversocket.accept()
     while True:
-        serverin = connection.recv(1024)
+        serverin = connection.recv(128)
         if not serverin: break
         if serverin:
             serverin = serverin.decode('utf-8')
-            server_comm(serverin)
-            serverout = 'string\n'
-            serverout = serverout.encode('utf-8')
+            serverout = server_comm(serverin)
         if serverout:
-            connection.send(serverout)
+            try:
+                serverout = serverout.encode('utf-8')
+                connection.send(serverout)
+            except:
+                break
     connection.close()
