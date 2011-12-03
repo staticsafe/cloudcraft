634dc94ffb299c726133e7c7e6355f49b1dae1bb
TingPing <tngpng@gmail.com>
2011-12-03 00:55:10 -0500

    fixed socket issues

--- python/main.py
+++ python/main.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
 import socket
+#import threading
 
 def server_start(memory='1024M', jar='../minecraft/minecraft_server.jar', cwd='../minecraft/'):
     if os.path.isfile('../minecraft/server.log.lck'):
@@ -12,38 +13,49 @@ def server_start(memory='1024M', jar='../minecraft/minecraft_server.jar', cwd='.
         executable = 'java -Xmx' + memory + ' -Xms' + memory + ' -jar ' + jar + ' nogui'
         global serverproc
         serverproc = subprocess.Popen(executable, cwd=cwd, startupinfo=startupinfo, stdin=subprocess.PIPE, universal_newlines=True)
-        global returncode
-        returncode = serverproc.returncode
+        print('started')
+        return serverproc
         
 def server_comm(serverin):
     if (serverin == 'start'):
         server_start()
-        return 'started'
+        return 'works'
     elif (serverin == 'terminate'):
         serverproc.terminate()
         os.remove('../minecraft/server.log.lck')
         return 'killed'
     else:
-        serverproc.communicate(serverin)
-        return 'sent'
+        try:
+            serverproc.communicate(serverin)
+            return 'sent'
+        except NameError as error:
+            return 'Server not running'
 
 #command={'start':server_start(), 'terminate':serverproc.terminate()}
 
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+serversocket.bind(('localhost', 1337))
+serversocket.listen(5)
+print('listening')
 while True:
-    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serversocket.bind(('localhost', 1337))
-    serversocket.listen(5)
     connection, address = serversocket.accept()
+    print('connected by ', address)
     while True:
         serverin = connection.recv(128)
         if not serverin: break
         if serverin:
             serverin = serverin.decode('utf-8')
+            print(serverin)
             serverout = server_comm(serverin)
+        if not serverout: break
         if serverout:
             try:
+                serverout = serverout + '\n'
+                print(serverout)
                 serverout = serverout.encode('utf-8')
                 connection.send(serverout)
-            except:
-                break
-    connection.close()
+            except OSError as error:
+                print(error)
+                pass
