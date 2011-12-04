f63773700055a985589d68a512719a581163dcc1
TingPing <tngpng@gmail.com>
2011-12-04 02:14:54 -0500

    fixed cwd issue, temp removed backup

--- python/main.py
+++ python/main.py
@@ -2,11 +2,9 @@ import subprocess
 import socket
 import zipfile
 import configparser
-from os import path, remove, rename, name
+from os import path, remove, rename, name, listdir
 from urllib.request import urlretrieve
 from filecmp import cmp
-
-update_config()
 
 def server_start():
     if isfile(mcdir + 'server.log.lck'):
@@ -21,7 +19,7 @@ def server_start():
         update_config()
         executable = 'java -Xmx' + memory + ' -jar ' + mcdir + jar + ' nogui'
         global serverproc
-        serverproc = subprocess.Popen(executable, mcdir=mcdir, startupinfo=startupinfo, stdin=subprocess.PIPE, universal_newlines=True)
+        serverproc = subprocess.Popen(executable, cwd=mcdir, startupinfo=startupinfo, stdin=subprocess.PIPE, universal_newlines=True)
         return 'Running'
     
 def server_comm(serverin):
@@ -34,17 +32,20 @@ def server_comm(serverin):
             return 'Terminated'
         if not serverproc:
             return 'Server not running'
-    elif (serverin == 'backup'):
-        if serverproc:
-            return 'Please close before backing up'
-        if not serverproc:
-            update_config()
-            try:
-                currentmap = file.open(mcdir + '/world/')
-                backupfile = zipfile.ZipFile(backupdir + 'world.zip', mode='w', compression=ZIP_DEFLATED)
-                file.write(currentmap)
-                file.close()
-            except: return 'Backup Failed'              
+##    elif (serverin == 'backup'):
+##        if serverproc:
+##            return 'Please close before backing up'
+##        if not serverproc:
+##            update_config()
+##            worlds = listdir(mcdir)
+##            try:
+##                for each world in worlds:
+##                    file.open(mcdir + world)
+##                    #currentmap = file.open(mcdir + '/world/')
+##                    backupfile = zipfile.ZipFile(backupdir + world + '.zip', mode='w', compression=ZIP_DEFLATED)
+##                    file.write(world)
+##                    file.close()
+##            except: return 'Backup Failed'              
     elif (serverin == 'update'):
         if serverproc:
             return 'Please close before updating'
@@ -64,7 +65,7 @@ def server_comm(serverin):
     else:
         try:
             serverproc.communicate(serverin)
-            return 'sent'
+            return 'Sent'
         except NameError as error:
             return 'Server not running'
 
@@ -77,6 +78,8 @@ def update_config():
     backupdir = ['backupdir']
     memory = ['memory']
     
+update_config()
+
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind(('localhost', 1337))
 serversocket.listen(5)
