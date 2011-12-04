81beae242365fd21c7e1179a4f7380f4ebe22476
TingPing <tngpng@gmail.com>
2011-12-03 21:14:39 -0500

    now uses config files and step toward bukkit compat

--- python/main.py
+++ python/main.py
@@ -1,18 +1,19 @@
 import subprocess
 import socket
 import zipfile
+import configparser
 from os import path, remove, rename
 from urllib.request import urlretrieve
 from filecmp import cmp
 
-def server_start(memory='1024M', jar='../minecraft/minecraft_server.jar', cwd='../minecraft/'):
-    if serverproc:
+def server_start(memory='1024M'):
+    if isfile(cwd + 'server.log.lck'):
         return 'already running'
     else:
         startupinfo = subprocess.STARTUPINFO()
         startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
         startupinfo.wShowWindow = subprocess.SW_HIDE
-        executable = 'java -Xmx' + memory + ' -Xms' + memory + ' -jar ' + jar + ' nogui'
+        executable = 'java -Xmx' + memory + ' -Xms' + memory + ' -jar ' + cwd + jar + ' nogui'
         global serverproc
         serverproc = subprocess.Popen(executable, cwd=cwd, startupinfo=startupinfo, stdin=subprocess.PIPE, universal_newlines=True)
         return 'Running'
@@ -31,7 +32,7 @@ def server_comm(serverin):
             return 'Please close before backing up'
         if not serverproc:
             try:
-                currentmap = file.open('../minecraft/world/')
+                currentmap = file.open(cwd + '/world/')
                 backupfile = zipfile.ZipFile('../backups/world.zip', mode='w', compression=ZIP_DEFLATED)
                 file.write(currentmap)
                 file.close()
@@ -45,8 +46,8 @@ def server_comm(serverin):
                 if cmp(newjar[0], jar):
                     return 'No updated available'
                 else:
-                    remove('../minecraft/minecraft_server.jar')
-                    rename(newjar[0], '../minecraft/minecraft_server.jar')
+                    remove(cwd + jar)
+                    rename(newjar[0], cwd + jar)
                     return 'Updated'
             except: return 'minecraft.net down'
     else:
@@ -55,6 +56,12 @@ def server_comm(serverin):
             return 'sent'
         except NameError as error:
             return 'Server not running'
+
+config = configparser.ConfigParser()
+config.read('profile.ini')
+mcargs = config['mcargs']
+cwd = mcargs['cwd']
+jar = mcargs['jar']
     
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind(('localhost', 1337))
