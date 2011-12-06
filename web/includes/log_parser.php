be8d4c4b1207401a9caa87ed8cb769eee3f8fd7e
TingPing <tngpng@gmail.com>
2011-12-05 22:32:55 -0500

    server up changed

--- web/includes/log_parser.php
+++ web/includes/log_parser.php
@@ -1,22 +1,20 @@
 <div class="sub">
+	<?php
+	if(file_exists("../minecraft/server.log.lck")) {
+		echo '<div class="server_status" style="color:green">Server: UP</div>';
+	}
+	else {
+		echo '<div class="server_status" style="color:red">Server: DOWN</div>';
+	}
+	?>
 	<div class="prompt">
 		<textarea id="output" readonly="readonly">
 		<?php
 			$file = file_get_contents("../minecraft/server.log");
 			$file = explode("\n",$file);
 			for($i = 0; $i < count($file); $i++) {
-				echo $file[$i]."\n\n\n\n";
+				echo $file[$i]."\n";
 			}
 		?></textarea>
-		
 	</div><!-- prompt -->
-	<?php
-		echo 'Server Status: ';
-		if(file_exists("../minecraft/server.log.lck")) {
-			echo '<div class="server_status" style="background-color:green"></div>';
-		}
-		else {
-			echo '<div class="server_status" style="background-color:red"></div>';
-		}
-		?>
 </div><!-- sub -->
