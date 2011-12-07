08dba95f37e705258c3ba1a50ab2ebe9c4738378
TingPing <tngpng@gmail.com>
2011-12-07 00:51:09 -0500

    updated

--- python/gui.py
+++ python/gui.py
@@ -1,37 +1,26 @@
 from tkinter import *
 from tkinter.ttk import *
-import subprocess
-
-class Application(Frame):
-    def start_server(self):
-        startupinfo = subprocess.STARTUPINFO()
-        startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-        startupinfo.wShowWindow = subprocess.SW_HIDE
-        server = ('python main.py')
-        try:
-            process = subprocess.Popen(server, startupinfo=startupinfo)
-        except:
-            return 'already running'
-        
-    def createWidgets(self):
-        self.QUIT = Button(self)
-        self.QUIT["text"] = "Quit"
-        self.QUIT["command"] = self.quit
-
-        self.QUIT.pack({"side": "left"})
-
-        self.start = Button(self)
-        self.start["text"] = "Start",
-        self.start["command"] = self.start_server
-
-        self.start.pack({"side": "left"})
-
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
-        self.pack()
-        self.createWidgets()
 
 root = Tk()
-app = Application(master=root)
-app.mainloop()
-root.destroy()
+    
+startbutton = Button()
+stopbutton = Button()
+webstartbutton = Button()
+serverstatus = Label()
+menubar = Menu(root)
+filemenu = Menu(menubar, tearoff=0)
+
+root.config(menu=menubar)
+startbutton.config(text='Start Server')
+stopbutton.config(text='Stop Server')
+webstartbutton.config(text='Start WebServer')
+
+filemenu.add_command(label='Quit')
+menubar.add_cascade(label='File', menu=filemenu)
+
+serverstatus.pack()
+startbutton.pack(side='right')
+stopbutton.pack(side='right')
+webstartbutton.pack(side='right')
+
+root.mainloop()
